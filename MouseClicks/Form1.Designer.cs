89820621edf49aa5637617e2e746f35f23f603d4
aud404_st <aud404_st@CR404-PC12>
2023-06-30 18:31:10 +0300

    mouse coordinates added//

--- MouseClicks/Form1.Designer.cs
+++ MouseClicks/Form1.Designer.cs
@@ -30,13 +30,14 @@
         {
             this.clicksCountText = new System.Windows.Forms.Label();
             this.clicksNumber = new System.Windows.Forms.Label();
+            this.mouseCoordinatesText = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // clicksCountText
             // 
             this.clicksCountText.AutoSize = true;
             this.clicksCountText.Font = new System.Drawing.Font("TechnicBold", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.clicksCountText.Location = new System.Drawing.Point(150, 98);
+            this.clicksCountText.Location = new System.Drawing.Point(472, 285);
             this.clicksCountText.Name = "clicksCountText";
             this.clicksCountText.Size = new System.Drawing.Size(246, 41);
             this.clicksCountText.TabIndex = 0;
@@ -46,23 +47,36 @@
             // 
             this.clicksNumber.AutoSize = true;
             this.clicksNumber.Font = new System.Drawing.Font("TechnicBold", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.clicksNumber.Location = new System.Drawing.Point(384, 98);
+            this.clicksNumber.Location = new System.Drawing.Point(706, 285);
             this.clicksNumber.Name = "clicksNumber";
             this.clicksNumber.Size = new System.Drawing.Size(38, 41);
             this.clicksNumber.TabIndex = 1;
             this.clicksNumber.Text = "0";
             // 
+            // mouseCoordinatesText
+            // 
+            this.mouseCoordinatesText.AutoSize = true;
+            this.mouseCoordinatesText.Font = new System.Drawing.Font("TechnicBold", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.mouseCoordinatesText.Location = new System.Drawing.Point(372, 326);
+            this.mouseCoordinatesText.Name = "mouseCoordinatesText";
+            this.mouseCoordinatesText.Size = new System.Drawing.Size(463, 62);
+            this.mouseCoordinatesText.TabIndex = 2;
+            this.mouseCoordinatesText.Text = "Mouse Coordinates: (x: ? ; y: ? )\nput mouse pointer on the window..";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(558, 254);
+            this.ClientSize = new System.Drawing.Size(1200, 700);
+            this.Controls.Add(this.mouseCoordinatesText);
             this.Controls.Add(this.clicksNumber);
             this.Controls.Add(this.clicksCountText);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Click += new System.EventHandler(this.Form1_onClick);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -72,5 +86,6 @@
 
         private Label clicksCountText;
         private Label clicksNumber;
+        private Label mouseCoordinatesText;
     }
 }
--- MouseClicks/Form1.cs
+++ MouseClicks/Form1.cs
@@ -1,3 +1,5 @@
+using System.Windows.Forms;
+
 namespace MouseClicks
 {
     public partial class Form1 : Form
@@ -11,6 +13,37 @@ namespace MouseClicks
         {
             var n = int.Parse(this.clicksNumber.Text);
             this.clicksNumber.Text = Convert.ToString(++n);
+        }
+
+        private String CoordinatesToString(MouseEventArgs e)
+        {
+            return "Mouse Coordinates: (x: " + e.X.ToString() + "; y: " + e.Y.ToString()+ ")";
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.mouseCoordinatesText.Text = CoordinatesToString(e);
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            String message = "";
+
+            if (e.Button == MouseButtons.Right)
+            {
+                message = "Right Click!";
+            }
+
+            if (e.Button == MouseButtons.Left)
+            {
+                message = "Left Click!";
+            }
+
+            message += "\n" + CoordinatesToString(e);
+
+            String caption = "Mouse Click Detected";
+
+            MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
